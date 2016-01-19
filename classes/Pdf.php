739790f8edbba80d5cc4043bee6b055179a23108
Gilles Hemmerlé <gilles.h@iabsis.com>
2016-01-19 22:55:29 +0100

    Add a method to set landscape mode

--- classes/Pdf.php
+++ classes/Pdf.php
@@ -7,7 +7,8 @@ namespace Igestis\Modules\Html2PdfConverter;
  *
  * @author Gilles Hemmerlé
  */
-class Pdf  {
+class Pdf
+{
     /**
      *
      * @var string Html content
@@ -43,25 +44,34 @@ class Pdf  {
      */
     private $footerHeight;
 
+    /**
+     *
+     * @var bool
+     */
+    private $landscape;
+
 
     const MODE_FORCE_DOWNLOAD = 1;
     const MODE_INLINE = 2;
     const MODE_WRITE = 4;
     const MODE_WRITE_AND_FORCE_DOWNLOAD = 8;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->bookmarks = false;
         $this->showHeader = false;
         $this->headerContent = "";
         $this->headerHeight = 20;
         $this->footerHeight = 20;
+        $this->landscape = false;
     }
 
     /**
      *
      * @param bool $bookmarks
      */
-    public function enableBookmarks($bookmarks = true) {
+    public function enableBookmarks($bookmarks = true)
+    {
         $this->bookmarks = (bool)$bookmarks;
     }
 
@@ -69,7 +79,8 @@ class Pdf  {
      *
      * @param string $headerContent
      */
-    public function setHeader($headerContent) {
+    public function setHeader($headerContent)
+    {
         $this->headerContent = $headerContent;
     }
 
@@ -77,7 +88,8 @@ class Pdf  {
      * Set header height in mm
      * @param int $headerHeight
      */
-    public function setHeaderHeight($headerHeight) {
+    public function setHeaderHeight($headerHeight)
+    {
         $this->headerHeight = (int)$headerHeight;
     }
 
@@ -85,7 +97,8 @@ class Pdf  {
      *
      * @param string $headerContent
      */
-    public function setFooter($footerContent) {
+    public function setFooter($footerContent)
+    {
         $this->footerContent = $footerContent;
     }
 
@@ -93,17 +106,22 @@ class Pdf  {
      * Set header height in mm
      * @param int $footerHeight
      */
-    public function setFooterHeight($footerHeight) {
+    public function setFooterHeight($footerHeight)
+    {
         $this->footerHeight = (int)$footerHeight;
     }
 
-
+    public function setLandscape($landscape = false)
+    {
+        $this->landscape = $landscape;
+    }
 
     /**
      * Add html to the pdf content
      * @param type $html
      */
-    public function writeHtml($html) {
+    public function writeHtml($html)
+    {
         $this->html .= $html;
     }
 
@@ -112,9 +130,11 @@ class Pdf  {
      * @param string $filename
      * @param string $mode Use the MODE_* constants or the same letter as fpdf library
      */
-    public function output($filename="your_file.pdf", $mode=self::MODE_FORCE_DOWNLOAD) {
+    public function output($filename="your_file.pdf", $mode=self::MODE_FORCE_DOWNLOAD)
+    {
         //die($this->html . $this->footerContent);
-        switch ($mode) {
+        switch ($mode)
+        {
             case self::MODE_FORCE_DOWNLOAD : case "D" :
                 // download PDF as file
 
@@ -171,7 +191,8 @@ class Pdf  {
 
                     \Igestis\Utils\Debug::FileLogger("create pdf into $dest");
                     $this->wkhtmltopdf($src, $dest);
-                } catch(\Exception $e) {
+                } catch(\Exception $e)
+                {
                     $this->Error($e->getMessage());
                 }
 
@@ -180,28 +201,30 @@ class Pdf  {
 
     }
 
-    private function Error($msg) {
+    private function Error($msg)
+    {
         throw new \Exception('Html2Pdf ERROR: '.$msg);
     }
 
-    private function wkhtmltopdf($src, $dest) {
+    private function wkhtmltopdf($src, $dest)
+    {
         $output = $return_var = null;
 
         $header = "";
-        if($this->headerContent) {
+        if ($this->headerContent) {
              $headerHtmlFile = sys_get_temp_dir() . "/" . uniqid() . ".htm";
              file_put_contents($headerHtmlFile, $this->headerContent);
              $header = " -T " . $this->headerHeight . "mm --header-html " . escapeshellarg($headerHtmlFile) . " --header-spacing '3' ";
         }
 
-        if($this->footerContent) {
+        if ($this->footerContent) {
             $footerHtmlFile = sys_get_temp_dir() . "/" . uniqid() . ".htm";
             file_put_contents($footerHtmlFile, $this->footerContent);
             $footer = " -B " . $this->footerHeight . "mm --footer-html " . escapeshellarg($footerHtmlFile) . " --footer-spacing '3' ";
 
         }
 
-        $exec = __DIR__ . "/../bin/wkhtmltopdf-$(uname -m) --encoding UTF-8 --toc-disable-back-links --toc-disable-links --disable-internal-links --disable-external-links $header $footer " . ($this->bookmarks ? " --outline " : "") .  escapeshellarg($src) . " " . escapeshellarg($dest) . " 2>&1 > /var/log/igestis/Html2PdfConverter/logfile";
+        $exec = __DIR__ . "/../bin/wkhtmltopdf-$(uname -m) " . ($this->landscape ? ' -O landscape ' : '') . "--encoding UTF-8 --toc-disable-back-links --toc-disable-links --disable-internal-links --disable-external-links $header $footer " . ($this->bookmarks ? " --outline " : "") .  escapeshellarg($src) . " " . escapeshellarg($dest) . " 2>&1 > /var/log/igestis/Html2PdfConverter/logfile";
 
         exec( $exec, $output, $return_var);
         //exec("echo " . escapeshellarg($this->html) . " | iconv -t iso-8859-1 -f utf-8 -o - | xvfb-run -a -s '-screen 0 640x480x16' wkhtmltopdf --encoding UTF-8 - " . escapeshellarg($dest) . " 2>&1 > /var/log/igestis/Html2PdfConverter/logfile", $output, $return_var);
