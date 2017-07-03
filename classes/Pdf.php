68b6162a29ee42e108984e8e5e2a3f7aafe1650c
Gilles Hemmerlé <giloux@gmail.com>
2017-07-03 19:17:54 +0200

    Removed debug

--- classes/Pdf.php
+++ classes/Pdf.php
@@ -133,35 +133,34 @@ class Pdf
     public function output($filename="your_file.pdf", $mode=self::MODE_FORCE_DOWNLOAD)
     {
         //die($this->html . $this->footerContent);
-        switch ($mode)
-        {
-            case self::MODE_FORCE_DOWNLOAD : case "D" :
+        switch ($mode) {
+            case self::MODE_FORCE_DOWNLOAD: case "D":
                 // download PDF as file
 
-				if (ob_get_contents()) {
-					$this->Error('Some data has already been output, can\'t send PDF file');
-				}
-				header('Content-Description: File Transfer');
-				if (headers_sent()) {
-					$this->Error('Some data has already been output to browser, can\'t send PDF file');
-				}
-				header('Cache-Control: private, must-revalidate, post-check=0, pre-check=0, max-age=1');
-				//header('Cache-Control: public, must-revalidate, max-age=0'); // HTTP/1.1
-				header('Pragma: public');
-				header('Expires: Sat, 26 Jul 1997 05:00:00 GMT'); // Date in the past
-				header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
-				// force download dialog
-				if (strpos(php_sapi_name(), 'cgi') === false) {
-					header('Content-Type: application/force-download');
-					header('Content-Type: application/octet-stream', false);
-					header('Content-Type: application/download', false);
-					header('Content-Type: application/pdf', false);
-				} else {
-					header('Content-Type: application/pdf');
-				}
-				// use the Content-Disposition header to supply a recommended filename
-				header('Content-Disposition: attachment; filename="'.basename($filename).'"');
-				header('Content-Transfer-Encoding: binary');
+                if (ob_get_contents()) {
+                    $this->Error('Some data has already been output, can\'t send PDF file');
+                }
+                header('Content-Description: File Transfer');
+                if (headers_sent()) {
+                    $this->Error('Some data has already been output to browser, can\'t send PDF file');
+                }
+                header('Cache-Control: private, must-revalidate, post-check=0, pre-check=0, max-age=1');
+                //header('Cache-Control: public, must-revalidate, max-age=0'); // HTTP/1.1
+                header('Pragma: public');
+                header('Expires: Sat, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+                header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
+                // force download dialog
+                if (strpos(php_sapi_name(), 'cgi') === false) {
+                    header('Content-Type: application/force-download');
+                    header('Content-Type: application/octet-stream', false);
+                    header('Content-Type: application/download', false);
+                    header('Content-Type: application/pdf', false);
+                } else {
+                    header('Content-Type: application/pdf');
+                }
+                // use the Content-Disposition header to supply a recommended filename
+                header('Content-Disposition: attachment; filename="'.basename($filename).'"');
+                header('Content-Transfer-Encoding: binary');
 
                 do {
                     $src = sys_get_temp_dir() . "/" . uniqid() . ".htm";
@@ -180,7 +179,7 @@ class Pdf
 
                 break;
 
-            case self::MODE_WRITE : case "F" :
+            case self::MODE_WRITE: case "F":
                 try {
                     do {
                         $src = sys_get_temp_dir() . "/" . uniqid() . ".htm";
@@ -189,16 +188,13 @@ class Pdf
                     $dest = $filename;
                     file_put_contents($src, $this->html);
 
-                    \Igestis\Utils\Debug::FileLogger("create pdf into $dest");
                     $this->wkhtmltopdf($src, $dest);
-                } catch(\Exception $e)
-                {
+                } catch (\Exception $e) {
                     $this->Error($e->getMessage());
                 }
 
                 break;
         }
-
     }
 
     private function Error($msg)
@@ -212,24 +208,22 @@ class Pdf
 
         $header = "";
         if ($this->headerContent) {
-             $headerHtmlFile = sys_get_temp_dir() . "/" . uniqid() . ".htm";
-             file_put_contents($headerHtmlFile, $this->headerContent);
-             $header = " -T " . $this->headerHeight . "mm --header-html " . escapeshellarg($headerHtmlFile) . " --header-spacing '3' ";
+            $headerHtmlFile = sys_get_temp_dir() . "/" . uniqid() . ".htm";
+            file_put_contents($headerHtmlFile, $this->headerContent);
+            $header = " -T " . $this->headerHeight . "mm --header-html " . escapeshellarg($headerHtmlFile) . " --header-spacing '3' ";
         }
 
         if ($this->footerContent) {
             $footerHtmlFile = sys_get_temp_dir() . "/" . uniqid() . ".htm";
             file_put_contents($footerHtmlFile, $this->footerContent);
             $footer = " -B " . $this->footerHeight . "mm --footer-html " . escapeshellarg($footerHtmlFile) . " --footer-spacing '3' ";
-
         }
 
         $exec = __DIR__ . "/../bin/wkhtmltopdf-$(uname -m) " . ($this->landscape ? ' -O landscape ' : '') . "--encoding UTF-8 --toc-disable-back-links --toc-disable-links --disable-internal-links --disable-external-links $header $footer " . ($this->bookmarks ? " --outline " : "") .  escapeshellarg($src) . " " . escapeshellarg($dest) . " 2>&1 > /var/log/igestis/Html2PdfConverter/logfile";
 
-        exec( $exec, $output, $return_var);
+        exec($exec, $output, $return_var);
         //exec("echo " . escapeshellarg($this->html) . " | iconv -t iso-8859-1 -f utf-8 -o - | xvfb-run -a -s '-screen 0 640x480x16' wkhtmltopdf --encoding UTF-8 - " . escapeshellarg($dest) . " 2>&1 > /var/log/igestis/Html2PdfConverter/logfile", $output, $return_var);
         //exec("xvfb-run -a -s '-screen 0 640x480x16' wkhtmltopdf " . escapeshellarg($src) . " " . escapeshellarg($dest) . " 2>&1 > /var/log/igestis/Html2PdfConverter/logfile", $output, $return_var);
         unlink($headerHtmlFile);
-        \Igestis\Utils\Debug::FileLogger($exec);
     }
 }
